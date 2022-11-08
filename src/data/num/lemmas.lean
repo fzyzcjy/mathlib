fbf582793c1c0444b5930246479e34e956a20ca8
negiizhao <egresf@gmail.com>
2022-11-09 01:27:31 +0800

    fix

--- src/data/num/lemmas.lean
+++ src/data/num/lemmas.lean
@@ -339,7 +339,6 @@ instance : linear_ordered_semiring num :=
   decidable_lt               := num.decidable_lt,
   decidable_le               := num.decidable_le,
   decidable_eq               := num.decidable_eq,
-  exists_pair_ne             := ⟨0, 1, dec_trivial⟩,
   ..num.comm_semiring, ..num.ordered_cancel_add_comm_monoid }
 
 @[simp, norm_cast] theorem add_of_nat (m n) : ((m + n : ℕ) : num) = m + n :=
@@ -1132,10 +1131,9 @@ instance : linear_ordered_comm_ring znum :=
   left_distrib     := by {transfer, simp [mul_add]},
   right_distrib    := by {transfer, simp [mul_add, mul_comm]},
   mul_comm         := by transfer,
-  exists_pair_ne   := ⟨0, 1, dec_trivial⟩,
   add_le_add_left  := by {intros a b h c, revert h, transfer_rw, exact λ h, add_le_add_left h c},
   mul_pos          := λ a b, show 0 < a → 0 < b → 0 < a * b, by {transfer_rw, apply mul_pos},
-  zero_le_one      := dec_trivial,
+  zero_lt_one      := dec_trivial,
   ..znum.linear_order, ..znum.add_comm_group, ..znum.add_monoid_with_one }
 
 @[simp, norm_cast] theorem cast_sub [ring α] (m n) : ((m - n : znum) : α) = m - n :=
